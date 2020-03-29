c0af36ba6ab90f7f9762bd346055f0f9bcc329ee
Lucas Le Ray <contact@lucas-le-ray.com>
2020-03-28 20:28:07 -0700

    fix: cors express

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ require('dotenv').config()
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
-app.use(cors({ origin: /^(http|https):\/\/crowdstar.xyz$/ }))
 app.use(router)
 app.use((error, req, res) => {
   res.status(500).send({ error })
