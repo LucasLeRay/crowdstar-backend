6359ce5df4ce0deff58f5f951891e53fbe68d57f
Lucas Le Ray <contact@lucas-le-ray.com>
2020-04-08 00:37:14 -0700

    fix: cron eslint

--- helpers/cron.js
+++ helpers/cron.js
@@ -1,44 +1,43 @@
-const dynamoDb = require("../helpers/dynamodb");
-const cron = require("node-cron");
+const cron = require('node-cron')
+const dynamoDb = require('./dynamodb')
 
-cron.schedule("0 0 */1 * * *", async () => {
+cron.schedule('0 0 */1 * * *', async () => {
+  const minDate6 = new Date()
+  const minDate12 = new Date()
 
-  let minDate6 = new Date();
-  let minDate12 = new Date();
-
-  minDate6.setHours(minDate6.getHours() - 6);
-  minDate12.setHours(minDate6.getHours() - 12);
+  minDate6.setHours(minDate6.getHours() - 6)
+  minDate12.setHours(minDate6.getHours() - 12)
 
   const params = {
     TableName: process.env.tableName,
     FilterExpression:
-      "#date < :minDate6 and (tier = :free or tier = :none) or \
-      #date < :minDate12 and tier = :std",
+      // eslint-disable-next-line max-len
+      '#date < :minDate6 and (tier = :free or tier = :none) or #date < :minDate12 and tier = :std',
     ExpressionAttributeValues: {
-      ":minDate6": minDate6.toString(),
-      ":minDate12": minDate12.toString(),
-      ":none": "NONE",
-      ":free": "FREE",
-      ":std": "STANDARD",
+      ':minDate6': minDate6.toString(),
+      ':minDate12': minDate12.toString(),
+      ':none': 'NONE',
+      ':free': 'FREE',
+      ':std': 'STANDARD',
     },
     ExpressionAttributeNames: {
-      "#date": "date",
+      '#date': 'date',
     },
-  };
+  }
 
   try {
-    const result = await dynamoDb.call("scan", params);
+    const result = await dynamoDb.call('scan', params)
     result.Items.map(async (item) => {
-      const params = {
+      const paramsDeletion = {
         TableName: process.env.tableName,
         Key: {
           boardId: item.boardId,
           name: item.name,
         },
-      };
-      return await dynamoDb.call("delete", params);
-    });
+      }
+      return dynamoDb.call('delete', paramsDeletion)
+    })
   } catch (error) {
-    console.log("Cron error ", error);
+    console.log('Cron error ', error)
   }
-});
+})
